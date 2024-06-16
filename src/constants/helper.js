8e85377f4bf66cd26e449e9ee640fbb19599c1a2
Lokendra Soni <lokendrasoni10@gmail.com>
2024-06-16 11:50:01 +0530

    fix: contract address

--- src/constants/helper.js
+++ src/constants/helper.js
@@ -1,4 +1,4 @@
 export const DesiredChainId = 1350216234;
 
-export const contractAddress = "0x525061A71786E8D9bB5193f8158c7A72AaA3c8E3";
+export const contractAddress = "0x17f2B4DeB46CfE6b864Fd3B397dA92F9AEe51D56";
 export const contractAddressTitanAITestNet = "0x525061A71786E8D9bB5193f8158c7A72AaA3c8E3";
