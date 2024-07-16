fcc858ce4177c0e3ace34b075278919324c8a9d9
Lokendra Soni <lokendrasoni10@gmail.com>
2024-07-16 18:24:10 +0530

    feat: image 2 3d advanced

--- src/apis/text23d/index.js
+++ src/apis/text23d/index.js
@@ -1,7 +1,26 @@
 import { auth } from "../";
 
-export const generate = async ({ prompt, image, type, quality }) => {
-    const res = await auth({
+export const generate = async ({ prompt, image, type, quality, file = null }) => {
+    let res;
+    if (file) {
+        const formData = new FormData();
+
+        formData.append("file", file);
+        formData.append("quality", quality);
+        formData.append("type", type);
+        res = await auth({
+            method: "POST",
+            url: "/3d-model-gen/generate",
+            data: formData,
+            options: {
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                },
+            },
+        });
+        return res?.data;
+    }
+    res = await auth({
         method: "POST",
         url: "/3d-model-gen/generate",
         data: { prompt, type, image, quality },
